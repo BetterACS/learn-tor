de8fb985d77450155b5bcd1f81d327dbac3f57ec
Mikiqueen0 <mikiqueen2004@gmail.com>
2025-01-04 10:42:15 +0700

    setup forum page roughly and fix typography

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import Test from '@/components/test';
+import TestMutation from '@/components/TestMutation';
+import Navbar from '@/components/navbar';
+import Sidebar from '@/components/sidebar';
+
+export {
+  Test,
+  TestMutation,
+  Navbar,
+  Sidebar,
+}
