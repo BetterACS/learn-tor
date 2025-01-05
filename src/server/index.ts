12d3ccc205828ebd380fea3b37c4a7f83c5d1b9c
peetnavapon <peetnavaponchaovanon@gmail.com>
2025-01-05 15:51:55 +0700

    add login in index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,12 @@
 import apiTest from './api/test';
 import register from './api/register';
+import login from  './api/login';
+
 import { createCallerFactory, router } from './trpc';
 export const appRouter = router({
 	...apiTest(),
     ...register(),
+    ...login(),
 });
 const createCaller = createCallerFactory(appRouter);
 
