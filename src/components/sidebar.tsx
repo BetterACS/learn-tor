46f5fba9015f1ef158a532a22e3685e8b945cf1f
Mikiqueen0 <mikiqueen2004@gmail.com>
2025-01-06 13:11:33 +0700

    fix sidebar scrollbar style

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -10,11 +10,14 @@ export default function Sidebar() {
     "ทุนการศึกษา",
     "การเตรียมตัวเรียนต่อต่างประเทศ",
     "เส้นทางอาชีพ",
-    "กิจกรรมและค่าย"
+    "กิจกรรมและค่าย",
+    "test",
+    "testststt",
+    "testestsseasd",
   ];
 
   return (
-    <div className="h-[calc(100vh-5.25rem)] w-full bg-monochrome-50 sticky overflow-y-auto scrollbar-none top-[5.25rem] flex flex-col divide-y divide-monochrome-200 items-center px-8 py-6 border-r border-monochrome-400">
+    <div className="h-[calc(100vh-5.25rem)] w-full bg-monochrome-50 sticky overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-monochrome-100 top-[5.25rem] flex flex-col divide-y divide-monochrome-200 items-center px-8 py-6 border-r border-monochrome-400">
       <div className="w-full flex flex-col py-6 first:pt-0 items-start">
         <Link href="/forum" className="w-full h-20 flex gap-3 items-center px-4 hover:bg-monochrome-100 transform duration-100 rounded-lg">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
