84089c6773895d9a75cd307753df725978b711cb
thiyada-kitt <pthiyada.kittiwithitkun@gmail.com>
2025-01-12 16:45:37 +0700

    fix icon footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import Link from "next/link";
 
 const Footer: React.FC = () => {
+  const socialLinks = [
+    { href: "https://www.instagram.com", imgSrc: "/images/instagram-logo.png", alt: "Instagram", ariaLabel: "Instagram" },
+    { href: "https://www.facebook.com", imgSrc: "/images/facebook-logo.png", alt: "Facebook", ariaLabel: "Facebook" },
+    { href: "https://www.twitter.com", imgSrc: "/images/twitter-logo.png", alt: "Twitter", ariaLabel: "Twitter" },
+    { href: "https://www.linkedin.com", imgSrc: "/images/linkedIn-logo.png", alt: "LinkedIn", ariaLabel: "LinkedIn" },
+  ];
+
   return (
     <div className="flex flex-col">
       <main className="flex-grow"></main>
 
       <footer className="bg-primary-600 py-6 text-monochrome-50">
         <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between">
-
           {/* Left Section - Logo */}
           <div className="flex flex-col items-start mb-6 md:mb-0">
             <img
@@ -24,31 +30,13 @@ const Footer: React.FC = () => {
             <div>
               <div className="text-headline-4 mb-4">Learntor</div>
               <ul className="space-y-2 text-headline-5">
-                <li>
-                  <Link href="/" className="hover:underline">
-                    FAQ
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    About Us
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    Our Services
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    Privacy Policy
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    Affiliate Program
-                  </Link>
-                </li>
+                {["FAQ", "About Us", "Our Services", "Privacy Policy", "Affiliate Program"].map((text) => (
+                  <li key={text}>
+                    <Link href="/" className="hover:underline">
+                      {text}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
 
@@ -56,36 +44,13 @@ const Footer: React.FC = () => {
             <div className="mt-6 md:mt-0">
               <div className="text-headline-4 mb-4">Link For Function</div>
               <ul className="space-y-2 text-headline-5">
-                <li>
-                  <Link href="/" className="hover:underline">
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    TCAS Info
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    Compare Courses
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/forum" className="hover:underline">
-                    Forum
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    TCAS Calculate
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="hover:underline">
-                    Chatbot
-                  </Link>
-                </li>
+                {["Home", "TCAS Info", "Compare Courses", "Forum", "TCAS Calculate", "Chatbot"].map((text) => (
+                  <li key={text}>
+                    <Link href="/" className="hover:underline">
+                      {text}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
@@ -94,37 +59,22 @@ const Footer: React.FC = () => {
           <div className="flex flex-col items-start">
             <div className="text-headline-4 mb-4">Contact Us</div>
             <div className="flex space-x-4">
-              <Link href="https://www.instagram.com" className="bg-white p-2 rounded-full" aria-label="Instagram">
-                <img
-                  src="/images/instagram-logo.png"
-                  alt="Instagram"
-                  className="h-10 w-10"
-                />
-              </Link>
-              <Link href="https://www.facebook.com" className="bg-white p-2 rounded-full" aria-label="Facebook">
-                <img
-                  src="/images/facebook-logo.png"
-                  alt="Facebook"
-                  className="h-10 w-10"
-                />
-              </Link>
-              <Link href="https://www.twitter.com" className="bg-white p-2 rounded-full" aria-label="Twitter">
-                <img
-                  src="/images/twitter-logo.png"
-                  alt="Twitter"
-                  className="h-10 w-10"
-                />
-              </Link>
-              <Link href="https://www.linkedin.com" className="bg-white p-2 rounded-full" aria-label="LinkedIn">
-                <img
-                  src="/images/linkedIn-logo.png"
-                  alt="LinkedIn"
-                  className="h-10 w-10"
-                />
-              </Link>
+              {socialLinks.map(({ href, imgSrc, alt, ariaLabel }) => (
+                <Link
+                  key={ariaLabel}
+                  href={href}
+                  className="bg-white rounded-full flex items-center justify-center h-12 w-12 sm:h-10 sm:w-10"
+                  aria-label={ariaLabel}
+                >
+                  <img
+                    src={imgSrc}
+                    alt={alt}
+                    className="h-8 w-8 sm:h-6 sm:w-6 object-contain"
+                  />
+                </Link>
+              ))}
             </div>
           </div>
-
         </div>
       </footer>
     </div>
