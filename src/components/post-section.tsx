349beb53aa2ab2b353a18cc8ce3e3a34b67f0b1c
peetnavapon <peetnavaponchaovanon@gmail.com>
2025-02-12 17:21:06 +0700

    Update post-section.tsx

--- src/components/post-section.tsx
+++ src/components/post-section.tsx
@@ -2,9 +2,11 @@
 import { Post, SortBy } from '@/components/index';
 import { trpc } from '@/app/_trpc/client';
 
-export default function PostSection() {
-  const { data: posts, isLoading, isError } = trpc.queryTopic.useQuery();
 
+export default function PostSection() {
+
+  const { data: posts, isLoading, isError } = trpc.queryTopic.useQuery();
+  console.log(posts)
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading posts</div>;
 
