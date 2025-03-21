1ca9a38f86b745ce8a50e1045e908db011ad0bbd
Jack <jessada.pranee@gmail.com>
2025-03-21 07:07:27 +0700

    fix error version

--- jest.config.ts
+++ jest.config.ts
@@ -1,27 +1,29 @@
 import type { Config } from '@jest/types'
 import nextJest from 'next/jest.js'
- 
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-    dir: './',
+  dir: './',
 })
- 
+
 // Add any custom config to be passed to Jest
 const config: Config.InitialOptions = {
-    coverageProvider: 'v8',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
-    extensionsToTreatAsEsm: [".ts", ".tsx"],
-    transformIgnorePatterns: ['node_modules/(?!(jose)/)','/node_modules/(?!react-markdown)'], // Transpile `jose`
-    transform: {
-      "^.+\\.(ts|tsx)$": "ts-jest",
-    },
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1', // Adjust this path if necessary
-      },
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  transformIgnorePatterns: [
+    // Update this line to include react-markdown and its dependencies
+    'node_modules/(?!(jose|react-markdown|micromark|mdast|unist|remark|rehype|unified|bail|character-entities|vfile)/)'],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^react-markdown$': '<rootDir>/__mocks__/react-markdown.tsx',
+  },
 }
- 
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
-
